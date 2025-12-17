933d1206cca01f8245fd0b4156c4413f055f5ad0
Neil Penman <neilpenman@gmail.com>
2025-12-17 20:44:29 +1000

    add point entry

---
+++ collect_app/src/main/java/au/smap/fieldTask/loaders/PointEntry.java
@@ -0,0 +1,10 @@
+package au.smap.fieldTask.loaders;
+
+/**
+ * This class holds the per-item location user trail data
+ */
+public class PointEntry {
+    public double lat;    // form or task
+    public double lon;
+    public long time;
+}
